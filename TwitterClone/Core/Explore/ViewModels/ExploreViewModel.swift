efbcc23e55fe0f0d7a8d85fdfd0e806ac075e029
OzgunEfe <ozgunefe.dev@gmail.com>
2024-07-17 20:25:17 +0100

    Search bar and filter users section done.

--- TwitterClone/Core/Explore/ViewModels/ExploreViewModel.swift
+++ TwitterClone/Core/Explore/ViewModels/ExploreViewModel.swift
@@ -9,6 +9,23 @@ import Foundation
 
 class ExploreViewModel: ObservableObject {
     @Published var users = [User]()
+    @Published var searchText = ""
+    
+    var searchableUsers: [User] {
+        // Burada searchbar text kontrolu yapiyorum. Eger text alani bossa kullanicilari gosteriyorum.
+        if searchText.isEmpty {
+            return users
+        // Eger kullanici bir sey yazmissa buna gore search ozelligini calistiyorum.
+        } else {
+            let lowercasedQuary = searchText.lowercased()
+            
+            return users.filter( {
+                $0.username.contains(lowercasedQuary) ||
+                $0.fullname.lowercased().contains(lowercasedQuary)
+            })
+        }
+    }
+    
     let service = UserService()
     
     init() {
