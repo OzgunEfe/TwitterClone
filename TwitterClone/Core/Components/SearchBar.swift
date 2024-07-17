4a2ee58c9da06de60b890bed36a8a4466d77da6a
OzgunEfe <ozgunefe.dev@gmail.com>
2024-07-17 19:45:03 +0100

    Created a search bar and fixed the navigation title bug.

---
+++ TwitterClone/Core/Components/SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  TwitterClone
+//
+//  Created by Ozgun Efe on 17/07/2024.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}
